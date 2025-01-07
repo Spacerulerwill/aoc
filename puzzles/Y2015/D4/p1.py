f32fa539fe2cdab213fd88d44b1a7e39cffa9dae
William Redding <williamdredding@proton.me>
2025-01-07 15:19:26 +0000

    2015 day 4

---
+++ puzzles/Y2015/D4/p1.py
@@ -0,0 +1,12 @@
+import hashlib
+from typing import Any
+
+
+def puzzle(input: str) -> Any:
+    input = input.strip()
+    i = 0
+    while True:
+        hash_hex_string = hashlib.md5((input + str(i)).encode()).hexdigest()
+        if hash_hex_string[:5] == "00000":
+            return i
+        i += 1
